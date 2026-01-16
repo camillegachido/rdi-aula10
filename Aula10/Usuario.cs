8b87b029ea09aa8afb68f14a25f80c86c71c90ed
ninahffbs <nina.hffs@gmail.com>
2026-01-16 20:59:04 -0300

    feat: adiciona classe Usuario

---
+++ Aula10/Usuario.cs
@@ -0,0 +1,44 @@
+namespace Aula10;
+
+public class Usuario
+{
+    public string Nome { get; private set; }
+    public string Telefone { get; private set; }
+    public List<Pedido> Pedidos { get; private set; }
+
+    public Usuario(string nome, string telefone)
+    {
+        Nome = nome;
+        Telefone = telefone;
+        Pedidos = new List<Pedido>();
+    }
+
+    public void FazerPedido(Restaurante restaurante, Pedido pedido)
+    {
+        if (restaurante == null || pedido == null)
+            return;
+
+        // associa o pedido ao usuário
+        pedido.Usuario = this;
+
+        // adiciona o pedido à lista do usuário
+        Pedidos.Add(pedido);
+
+        // adiciona o pedido ao restaurante
+        restaurante.Pedidos.Add(pedido);
+    }
+
+    public void VerPedidosAnteriores()
+    {
+        if (Pedidos.Count == 0)
+        {
+            Console.WriteLine("Nenhum pedido realizado.");
+            return;
+        }
+
+        foreach (var pedido in Pedidos)
+        {
+            Console.WriteLine(pedido);
+        }
+    }
+}
