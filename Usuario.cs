42dd1b35c106f05a1236c0852a9b62fd2d839bb0
Eloá <eloa.andreatta@gmail.com>
2026-01-16 21:14:10 -0300

    adição da classe usurario

--- Usuario.cs
+++ Usuario.cs
@@ -13,6 +13,27 @@ public class Usuario
         Telefone = telefone;
     }
 
+    public void FazerPedido(Restaurante restaurante, Pedido pedido)
+        {
+            Pedidos.Add(pedido);
+            restaurante.Pedidos.Add(pedido);
+        }
 
+        public void VerPedidosAnteriores()
+        {
+            if (Pedidos.Count == 0)
+            {
+                Console.WriteLine("Nenhum pedido realizado.");
+                return;
+            }
 
+            foreach (var pedido in Pedidos)
+            {
+                pedido.FinalizarPedido();
+                Console.WriteLine("--");
+            }
+        }
 }
+
+
+
